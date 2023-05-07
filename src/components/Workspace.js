eac5aabb6e415d34e47369d515a095a6a2a09e00
Yaresko Vlad <yareskofamily@gmail.com>
2023-05-07 14:08:44 +0300

    Создал проект, добавил компоненты

---
+++ src/components/Workspace.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Workspace() {
+  return (
+    <div className="workspace">
+      <h2>Workspace Component</h2>
+    </div>
+  );
+}
+
+export default Workspace;
